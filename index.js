5016fcf9ddc0d8f65565c53b97fabea6260e42f9
JSONRete <jsonrete@gmail.com>
2021-07-26 07:26:30 -0400

    commit tree fab221cda35f07c10ac633de13e7c70d7af59f5f

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+
+  const hour = parseInt(time, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+
+// 1) Returns Good Morning before 12pm
+// 2) Returns Good Afternoon between 12pm and 5pm
+// 3) Returns Good Evening after 5pm
+
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  document.getElementById("greeting").textContent = msg;
+}
+// 4) updates the DOM with an appropriate message
